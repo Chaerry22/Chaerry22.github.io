59d6da631945a9938e88b474048e7fd5bb7449a1
Chaerry22 <dchae22@gmail.com>
2024-07-27 11:37:11 -0400

    first
    first

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    
+    if (menu.classList.contains("open")) {
+      menu.style.maxHeight = "0";
+    } else {
+      menu.style.maxHeight = menu.scrollHeight + "px";
+    }
+    
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
